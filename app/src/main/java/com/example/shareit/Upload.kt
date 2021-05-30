3f6e1bb93dc890201b251a72a6207bfe9186b409
Nobuuuuuu <lifendyel@gmail.com>
2021-05-30 19:14:33 +0200

    :bug: Fix Drop Activité Upload

--- app/src/main/java/com/example/shareit/Upload.kt
+++ app/src/main/java/com/example/shareit/Upload.kt
@@ -1,14 +1,13 @@
 package com.example.shareit
 
 class Upload {
-    private var name: String? = null
-    private var imageUrl: String? = null
-
+    lateinit var name: String
+    lateinit var imageUrl: String
     constructor() {
         // Vide
     }
 
-    constructor(name: String, imageUrl: String?) {
+    constructor(name: String, imageUrl: String) {
         var name = name
         if (name.trim { it <= ' ' } == "") {
             name = "No Name"
