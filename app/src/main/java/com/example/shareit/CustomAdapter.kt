b29da7b3d14f9ba324cd04cac6ebff28b7f7f152
Nobuuuuuu <lifendyel@gmail.com>
2021-06-01 09:16:32 +0200

    :sparkles: Ajout focus image

--- app/src/main/java/com/example/shareit/CustomAdapter.kt
+++ app/src/main/java/com/example/shareit/CustomAdapter.kt
@@ -1,12 +1,15 @@
 package com.example.shareit
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
+
 
 class CustomAdapter(private var images:List<Image>):
     RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
@@ -29,7 +32,16 @@ class CustomAdapter(private var images:List<Image>):
         val item = images[position]
         Picasso.get().load(item.imageUrl).into(viewHolder.imageView)
         viewHolder.imageTitleView.text = item.name
-        viewHolder.imageDescriptionView.text = item.description    }
+        viewHolder.imageDescriptionView.text = item.description
+
+        viewHolder.itemView.setOnClickListener{
+            Toast.makeText(it.context, "Click " + item.name, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(it.context, ImageDisplayActivity::class.java)
+            intent.putExtra("EXTRA_IMAGEVIEW_URL", item.imageUrl)
+            it.context.startActivity(intent)
+        }
+    }
 
     class ViewHolder(view: View):RecyclerView.ViewHolder(view) {
         val imageView: ImageView = view.findViewById(R.id.imageView)
